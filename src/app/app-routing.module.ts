e3d80d1db8ec2950f8eddd6a8e313ad39c433187
jleon253 <jleon253@gmail.com>
2020-09-21 15:53:14 -0500

    Configurando routing
    Se crean los componentes que usara el sistema de rutas
    
    Se condigura archivo modules para integrar los componentes creados
    
    Se configura archivo routing con las rutas a utilizar
    
    Se ajusta index.html y app.component para dar comienzo al proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { TheoryComponent } from './components/pages/theory/theory.component';
+import { CommentsComponent } from './components/pages/comments/comments.component';
+import { ErrorComponent } from './components/pages/error/error.component';
+import { NotfoundComponent } from './components/pages/notfound/notfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'theory', component: TheoryComponent},
+  {path: 'comments', component: CommentsComponent},
+  {path: 'error', component: ErrorComponent},
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { CommentsComponent } from './components/pages/comments/comments.component';
+import { NotfoundComponent } from './components/pages/notfound/notfound.component';
+import { ErrorComponent } from './components/pages/error/error.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { TheoryComponent } from './components/pages/theory/theory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CommentsComponent,
+    NotfoundComponent,
+    ErrorComponent,
+    NavbarComponent,
+    TheoryComponent,
   ],
   imports: [
     BrowserModule,
