80376ebcb220c328b09ba2b0f154a144f9da8087
jleon253 <jleon253@gmail.com>
2020-09-22 23:19:35 -0500

    Ajustando secciones en HomePage
    Se ajusta responsive de secciones
    
    Se agrega nueva seccion
    
    Se agrega contenido multimedia a implementar

--- src/app/components/shared/blockplus/blockplus.component.ts
+++ src/app/components/shared/blockplus/blockplus.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blockplus',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockplusComponent implements OnInit {
 
-  constructor() { }
+  @Input() color: string;
+  fill: string;
+  
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.fill = (this.color === 'orange') ? 'rgb(236,78,32)' : 'rgb(255,255,255)';
   }
 
 }
