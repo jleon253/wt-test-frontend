85194692b83c6636756e0088056dc8b3e5009c0c
jleon253 <jleon253@gmail.com>
2020-09-23 17:10:38 -0500

    Se agrega contenido a TheoryPage
    Se ajusta TheoryPage en contenido y estilo visual
    
    Se ajusta navegacion para ir a TheroyPage y no mostrar el menu en ella
    
    Se mueve el footer a app-component

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterContentChecked {
 
   menu: any;
   menuIcon: any;
+  showMenu: boolean = true;
 
-  constructor() { }
+  constructor(private _router: Router) { 
+    console.log(_router);
+  }
 
   ngOnInit(): void {
     this.menu = document.getElementById('wt-menu');
     this.menuIcon = document.getElementById('wt-navbar__menuIcon');
   }
+
+  ngAfterContentChecked(): void{
+    this.showMenu = (this._router.url === "/") ? true : false;
+  }
+
 
   toggleMenu() {
     this.menu.classList.toggle('open-menu');
