bcf7527b25abe30dfe772277aa9de9540cf8f4f4
jleon253 <jleon253@gmail.com>
2020-09-23 22:54:59 -0500

    Se agregan animaciones
    Se implementa la libreria animations
    
    Se agrega codigo JS para llamar las animaciones al hacer scroll
    
    Se agrega codigo css para otras animaciones
    
    Se aplican ajustes pequenos en los blurbs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WT-Test-FrontEnd';
+export class AppComponent implements OnInit {
+  elements: any = [];
+  windowHeight: any;
+
+  ngOnInit(): void {
+    this.initial();
+    this.checkPosition();
+    window.addEventListener('scroll', this.initial);
+    window.addEventListener('scroll', this.checkPosition);
+  }
+
+  initial() {
+    this.elements = document.querySelectorAll('.hidden');
+    this.windowHeight = window.innerHeight;
+  }
+
+  checkPosition() {
+    if (this.elements.length > 0) {
+      for (let i = 0; i < this.elements.length; i++) {
+        let element = this.elements[i];
+        let positionFromTop = this.elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - this.windowHeight <= 0) {
+          element.classList.remove('hidden');
+          element.classList.add('animate__animated');
+          element.classList.add('animate__backInDown');
+        }
+      }
+    }
+  }
 }
